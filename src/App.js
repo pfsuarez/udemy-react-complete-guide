2144793c7a0baa47b1f80730fdda13b0365686fd
Pedro Suarez <pfsuarez@gmail.com>
2021-02-15 12:26:14 -0300

    Added StyleRoot

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //import React, { Component } from 'react';
 import React, { useState } from 'react';
 
-import Radium from "radium";
+import Radium, {StyleRoot} from "radium";
 
 import './App.css';
 import Person from "./Person/Person";
@@ -132,16 +132,18 @@ const App = props => {
   }
 
   return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p className={classes.join(" ")}>This is really working!</p>
-      <button
-        style={style}
-        onClick={togglePersonHandler}>
-        Toggle Person
-      </button>
-      {persons}
-    </div>
+    <StyleRoot>
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(" ")}>This is really working!</p>
+        <button
+          style={style}
+          onClick={togglePersonHandler}>
+          Toggle Person
+        </button>
+        {persons}
+      </div>
+    </StyleRoot>
   );
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,14 @@ import Radium from "radium";
 import "./Person.css";
 
 const Person = (props) => {
+    const style = {
+        "@media (min-width: 500px)" : {
+            width:"400px"
+        }
+    };
+
     return (
-        <div className="Person">
+        <div className="Person" style={style}>
             <p onClick={props.click}>Name: {props.name} Age: {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}></input>
