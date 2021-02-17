337b8cfb1d5a8d9008ddd367640a74d3e15d4023
Pedro Suarez <pfsuarez@gmail.com>
2021-02-17 10:03:24 -0300

    Setting up the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Empty Template</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
 }
 
 export default App;
