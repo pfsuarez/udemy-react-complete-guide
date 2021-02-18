9455ea4329d13f03e2a1066a4492c46c3b323993
Pedro Suarez <pfsuarez@gmail.com>
2021-02-18 06:49:20 -0300

    Implementing Burger builder Added Burger ingredient component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Auxiliary";
+
+class BugerBuilder extends Component {
+    render() {
+        return(
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BugerBuilder;
