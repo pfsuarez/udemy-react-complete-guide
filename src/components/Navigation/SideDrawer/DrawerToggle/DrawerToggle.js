ce0244f414198c44593ef8b640aa4cc9003bc4ad
Pedro Suarez <pfsuarez@gmail.com>
2021-02-19 08:48:02 -0300

    Implementing DrawerToggle component

---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,13 @@
+import classes from "./DrawerToggle.module.css";
+
+const drawerToggle = props => (
+    <div
+        className={classes.DrawerToggle}
+        onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+);
+
+export default drawerToggle;
