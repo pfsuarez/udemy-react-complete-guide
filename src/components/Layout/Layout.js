4dd3efd530593f26ec7524816061ed21e6e0aa1e
Pedro Suarez <pfsuarez@gmail.com>
2021-02-19 08:23:18 -0300

    Implementing SideDrawer component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Layout.module.css";
 import Aux from "../../hoc/Auxiliary";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = props => {
-    return (
-        <Aux>
-            <Toolbar />
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-        </Aux>
-    );
+class Layout extends Component {
+    state= {
+        showSideDrawer: true
+    };
+
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
 }
 
-export default layout;
+export default Layout;
