8f9328d3a00e64a816b1c94325fe059c879b1c99
Pedro Suarez <pfsuarez@gmail.com>
2021-03-10 19:33:45 +0100

    handling 404 case

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -37,7 +37,8 @@ class Blog extends Component {
                 <Switch>
                     {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
                     <Route path="/posts" component={Posts} />
-                    <Redirect from="/" to="/posts" />
+                    {/* <Redirect from="/" to="/posts" /> */}
+                    <Route render={() => <h1>Not Found</h1>} />
                 </Switch>
             </div>
         );
