ea47926d7a5150a936fc745bc2cc0699d4194782
Pedro Suarez <pfsuarez@gmail.com>
2021-03-12 12:01:35 +0100

    Navigating back & to next page

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -14,12 +14,12 @@ const checkoutSummary = props => {
             </div>
             <Button
                 btnType="Danger"
-                clicked>
+                clicked={props.checkoutCancelled}>
                 CANCEL
             </Button>
             <Button
                 btnType="Success"
-                clicked>
+                clicked={props.checkoutContinued}>
                 CONTINUE
                 </Button>
         </div>
