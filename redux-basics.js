853c45d41229bbb10e39741c2621997387c4b3d6
Pedro Suarez <pfsuarez@gmail.com>
2021-03-15 20:30:27 +0100

    Dispatching Actions

--- redux-basics.js
+++ redux-basics.js
@@ -7,6 +7,19 @@ const initialState = {
 
 //Reducer
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
 
     return state;
 };
@@ -17,9 +30,9 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 
 
-
 //Dispatching Actions
-
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
 
 
 //Subscription
