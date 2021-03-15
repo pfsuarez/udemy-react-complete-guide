bdd9e01bfb3267b01f8b8629d9c2fb92b236cbd3
Pedro Suarez <pfsuarez@gmail.com>
2021-03-15 20:21:28 +0100

    Setting up reducer and store

---
+++ redux-basics.js
@@ -0,0 +1,25 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0
+};
+
+//Reducer
+const rootReducer = (state = initialState, action) => {
+
+    return state;
+};
+
+
+
+//Store
+const store = createStore(rootReducer);
+
+
+
+//Dispatching Actions
+
+
+
+//Subscription
