34d6ae231a834af40ed339f4dae2790d2935ffcb
Pedro Suarez <pfsuarez@gmail.com>
2021-03-16 16:13:15 +0100

    Outsourcing Action Types

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from './actions';
+
 const initialState = {
     counter: 0,
     results: []
@@ -5,33 +7,33 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'INCREMENT':
+        case actionTypes.INCREMENT:
             const newState = Object.assign({}, state); //clone object in inmutable way
             return {
                 ...state, //spread operator
                 counter: state.counter + 1
             };
-        case 'DECREMENT':
+        case actionTypes.DECREMENT:
             return {
                 ...state,
                 counter: state.counter - 1
             };
-        case 'ADD':
+        case actionTypes.ADD:
             return {
                 ...state,
                 counter: state.counter + action.val
             };
-        case 'SUBTRACT':
+        case actionTypes.SUBTRACT:
             return {
                 ...state,
                 counter: state.counter - action.val
             };
-        case 'STORE_RESULT':
+        case actionTypes.STORE_RESULT:
             return {
                 ...state,
                 results: state.results.concat({ id: new Date(), value: state.counter })
             };
-        case 'DELETE_RESULT':
+        case actionTypes.DELETE_RESULT:
             // const newArray = [...state.results];
             // newArray.splice(action.resultElId, 1);
 
