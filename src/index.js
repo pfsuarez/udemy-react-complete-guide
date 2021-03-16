16b1270b665d175baff00f124d977d8cd8620020
Pedro Suarez <pfsuarez@gmail.com>
2021-03-16 17:12:42 +0100

    Created reducer. Created store from reducer. Passed store to Provider component.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import appReducer from "./store/reducer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const appStore = createStore(appReducer);
+
+ReactDOM.render(<Provider store={appStore}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
