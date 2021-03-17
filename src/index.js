d337c7aaa0dacf85545b583d0e387209b5ba4733
Pedro Suarez <pfsuarez@gmail.com>
2021-03-17 10:44:30 +0100

    Basic Redux Setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import './index.css';
 import App from './App';
+import reducer from "./store/reducer";
 import reportWebVitals from './reportWebVitals';
+
+const appReducer = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={appReducer}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store/reducer.js
@@ -0,0 +1,20 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 4
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            
+            break;
+    
+        case actionTypes.REMOVE_INGREDIENT:
+            break;
+    }
+    return state;
+};
+
+export default reducer;
