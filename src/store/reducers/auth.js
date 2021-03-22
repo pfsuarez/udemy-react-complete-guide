441fdd2bfdd752da882f083370e3e6aa08e4801d
Pedro Suarez <pfsuarez@gmail.com>
2021-03-22 12:29:59 +0100

    Logging Users Out

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -25,9 +25,19 @@ const authFail = (state, action) => {
     return updateObject(state, { error: action.error, loading: false });
 };
 
+const authLogout = (state, action) => {
+    return updateObject(state, {
+        token: null,
+        userId: null,
+        error: null,
+        loading: false
+    });
+};
+
 const authReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.AUTH_START: return authStart(state, action);
+        case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
         case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
         case actionTypes.AUTH_FAIL: return authFail(state, action);
 
