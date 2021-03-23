44f7830d812d938f38ed093aeee0d940fd55efbf
Pedro Suarez <pfsuarez@gmail.com>
2021-03-23 17:22:50 +0100

    Creating the Basic React Application

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component  } from "react";
+import { Link, Route } from "react-router-dom";
+
+import asyncComponent from "./hoc/asyncComponent";
+
+import Users from "./containers/Users";
+
+const asyncPizza = asyncComponent(()=> {
+    return import('./containers/Pizza');
+});
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <Link to="/">Users</Link>
+                    <Link to="/pizza">Users</Link>
+                </div>
+                <div>
+                    <Route path="/" exact component={Users} />
+                    <Route path="/pizza" component={asyncPizza} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App; 
