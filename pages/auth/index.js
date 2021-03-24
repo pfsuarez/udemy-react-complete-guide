1e3c00cd62ec7aab47e133bc55f431107646da21
Pedro Suarez <pfsuarez@gmail.com>
2021-03-24 10:22:22 +0100

    Understanding the Basics

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Link from "next/link";
 
 const authIndexPage = () => (
     <div>
         <h1>The Auth Main Page</h1>
+        <p>Go to <Link href="/">Main Page</Link></p>
     </div>
 );
 
