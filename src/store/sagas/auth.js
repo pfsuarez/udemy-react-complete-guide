8e9cb093e82b9fb7d284c248ef76f23da27809d0
Pedro Suarez <pfsuarez@gmail.com>
2021-03-26 12:30:52 +0100

    Diving Deeper into Sagas

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,13 +1,13 @@
-import { put, delay } from "redux-saga/effects";
+import { put, delay, call } from "redux-saga/effects";
 import axios from "axios";
 import * as actionTypes from "../actions/actionTypes";
 import * as actions from '../actions/index'
 import * as webApiKey from "../../firebaseWebApiKey";
 
 export function* logoutSaga(action) {
-    yield localStorage.removeItem('token');
-    yield localStorage.removeItem('expirationDate');
-    yield localStorage.removeItem('userId');
+    yield call([localStorage, 'removeItem'], 'token');
+    yield call([localStorage, 'removeItem'], 'expirationDate');
+    yield call([localStorage, 'removeItem'], 'userId');
 
     yield put(actions.logoutSucceed());
 }
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,14 +1,16 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, all, takeLatest } from "redux-saga/effects";
 import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from "./auth";
 import { initIngredientSaga } from "./burgerBuilder";
 import { purchaseBurgerSaga, fetchOrdersSaga } from "./order";
 import * as actionTypes from '../actions/actionTypes';
 
 export function* watchAuth() {
-    yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
-    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
-    yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
-    yield takeEvery(actionTypes.AUHT_CHECK_INITIAL_STATE, authCheckStateSaga);
+    yield all([
+        takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
+        takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+        takeEvery(actionTypes.AUTH_USER, authUserSaga),
+        takeEvery(actionTypes.AUHT_CHECK_INITIAL_STATE, authCheckStateSaga)
+    ]);
 }
 
 export function* wathBurgerBuilder() {
@@ -16,6 +18,6 @@ export function* wathBurgerBuilder() {
 }
 
 export function* watchOrder() {
-    yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+    yield takeLatest(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
     yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
