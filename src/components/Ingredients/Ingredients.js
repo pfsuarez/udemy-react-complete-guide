169fccfcedcdeea0684a4d478e909e99e9a6c6e7
Pedro Suarez <pfsuarez@gmail.com>
2021-03-29 11:05:52 +0200

    Understanding useReducer()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,12 +1,27 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useReducer, useEffect, useCallback, useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from "./IngredientList";
 import Search from './Search';
 import ErrorModal from "../UI/ErrorModal";
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter(x => x.id !== action.id);
+
+    default:
+      throw new Error('');
+  }
+};
+
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  //const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
@@ -24,24 +39,35 @@ const Ingredients = () => {
       return response.json();
     }).then(responseData => {
       setIsLoading(false);
-      setUserIngredients(prevIngredients => [
-        ...prevIngredients,
-        {
+      // setUserIngredients(prevIngredients => [
+      //   ...prevIngredients,
+      //   {
+      //     id: responseData.name,
+      //     ...ingredient
+      //   }
+      // ]);
+      dispatch({
+        type: 'ADD',
+        ingredient: {
           id: responseData.name,
           ...ingredient
         }
-      ]);
+      });
     });
   };
 
   const removeIngredientHandler = id => {
     setIsLoading(true);
-    fetch(`https://react-hooks-update-11-default-rtdb.firebaseio.com/ingredients/${id}.jso`, {
+    fetch(`https://react-hooks-update-11-default-rtdb.firebaseio.com/ingredients/${id}.json`, {
       method: 'DELETE'
     })
       .then(response => {
         setIsLoading(false);
-        setUserIngredients(prevIngredients => prevIngredients.filter(ing => ing.id !== id));
+        // setUserIngredients(prevIngredients => prevIngredients.filter(ing => ing.id !== id));
+        dispatch({
+          type:'DELETE',
+          id: id
+        })
       })
       .catch(error => {
         setError(error.message);
@@ -49,7 +75,11 @@ const Ingredients = () => {
   };
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
-    setUserIngredients(filteredIngredients);
+    // setUserIngredients(filteredIngredients);
+    dispatch({
+      type: 'SET',
+      ingredients: filteredIngredients
+    })
   }, []);
 
   const clearErrorHandler = () => {
