2e2d45f64a7015b90fd79fff261d214630e47457
Pedro Suarez <pfsuarez@gmail.com>
2021-03-30 12:18:49 +0200

    initial project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-function App() {
+import Navigation from './components/Nav/Navigation';
+import ProductsPage from './containers/Products';
+import FavoritesPage from './containers/Favorites';
+
+const App = props => {
   return (
-    <div className="App">
-      <h1>Empty Template</h1>
-    </div>
+    <React.Fragment>
+      <Navigation />
+      <main>
+        <Route path="/" component={ProductsPage} exact />
+        <Route path="/favorites" component={FavoritesPage} />
+      </main>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
