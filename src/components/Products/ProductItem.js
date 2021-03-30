9ab73fc5406d07c30b9e47dd27d5c3c4ec8aec07
Pedro Suarez <pfsuarez@gmail.com>
2021-03-30 12:37:49 +0200

    Alternative: Using the Context API

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useContext } from 'react';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
 import { toggleFav } from '../../store/actions/products';
+import { ProductsContext } from "../../context/products-context";
 
 const ProductItem = props => {
-  const dispatch = useDispatch();
+  //const dispatch = useDispatch();
 
   const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
+    //dispatch(toggleFav(props.id));
   };
 
   return (
